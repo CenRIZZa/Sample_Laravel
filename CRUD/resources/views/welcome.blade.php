e1aa8c05d47f620c50898d126766c73eb4483453
CenRIZZa <cenizamaki@gmail.com>
2025-03-26 23:57:09 +0800

    First commit

---
+++ CRUD/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Borrowing System</title>
+</head>
+<body>
+    <h1>Welcome to Borrowing System</h1>
+    <p>Click to view the list of Items</p>
+
+
+    <a href = "" class="btn">
+        View Items
+    </a>
+</body>
+</html>
